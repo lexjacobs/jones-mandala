d5a416f2246c1104a80e7370a7fa5eeb0a6fd82b
Glitch (patch-reading) <none>
2018-04-16 08:43:51 +0000

    📼🌇 Checkpoint ./README.md:300783/916 ./server.js:300783/839 ./package.json:300783/1567

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 'run strict'
-const http = require('http')
-console.log('hello')
+const express = require('express')
+const app = express()
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile('src/index.html',{root:"."}))
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
