bff8070dce5e4892f685a27fb8bdb7312b30185d
Alex Jacobs <lex.jacobs@gmail.com>
2018-04-16 15:32:39 +0000

    🔮🎸 Checkpoint ./README.md:300783/86 ./server.js:300783/55

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'run strict'
 const express = require('express')
 const app = express()
-//app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', (req, res) => res.sendFile('src/index.html',{root:"."}))
 app.get('/bundle', (req, res) => res.sendFile('src/output/bundle.js',{root:"."}))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
