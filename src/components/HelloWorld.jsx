0f6ff6c0d5ed523c8e253c153257119bfd4c4431
Glitch (patch-reading) <none>
2018-04-16 09:05:39 +0000

    🏂🚂 Checkpoint ./src/components/HelloWorld.jsx:300783/25

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -3,7 +3,7 @@ export class HelloWorld extends Component {
     render() {
         return (
             <div className="hello-world">
-                <h1>Hello World</h1>
+                <h1>Hello, {this.props.name}</h1>
             </div>
         );
     }
