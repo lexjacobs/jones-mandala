21b90786b03a2a646a2785cd6a4381979e9de9c7
Alex Jacobs <lex.jacobs@gmail.com>
2018-04-16 09:16:39 +0000

    💡🌷 Checkpoint ./src/index.jsx:300783/80 ./README.md:300783/211

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-## Hello
+## Instructions
+    1) Open the console via the `Advanced Options`
+    2) Type `pnpm run build` and hit ENTER
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './components/HelloWorld';
 import './styles/app.scss';
+
+const element = <HelloWorld name="Sara" />;
 ReactDOM.render(
-   <HelloWorld />,
+   element,
    document.getElementById('app')
 );
