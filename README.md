374220ec8afa618de2aadb2845a16807db1b746f
Glitch (react-webpack-babel-sass) <none>
2018-04-16 14:20:52 +0000

    🏯💼 Checkpoint ./src/index.html:300783/38 ./src/components/HelloWorld.jsx:300783/112 ./package.json:300783/275 ./README.md:300783/34

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Instructions
+    1) Make a custom change to 
     1) Open the console via the `Advanced Options`
     2) Type `pnpm run build` and hit ENTER
--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -3,7 +3,7 @@ export class HelloWorld extends Component {
     render() {
         return (
             <div className="hello-world">
-                <h1>Hello, {this.props.name}</h1>
+                <h1>Enough Babel! Don't over React! Turn down the SASS! ... Oh, hello {this.props.name}! Welcome to the Webpack!</h1>
             </div>
         );
     }
