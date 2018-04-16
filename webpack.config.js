76caed3aba9067a7ebc6a61b23164710ba63e572
Glitch (react-webpack-babel-sass) <none>
2018-04-16 17:25:16 +0000

    😽💶 Checkpoint ./webpack.config.js:300783/48

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 // This library allows us to combine paths easily
 const path = require('path');
 module.exports = {
-   entry: path.resolve(__dirname, 'src', 'index.jsx'),
+   entry: path.resolve('src', '../index.jsx'),
    output: {
-      path: path.resolve(__dirname, 'src/output'),
+      path: path.resolve('../src/output'),
       filename: 'bundle.js'
    },
    resolve: {
