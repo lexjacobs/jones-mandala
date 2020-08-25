8e25b6e6c8901b3d078cb45897effd8d5c76f214
Glitch (react-webpack-babel-sass) <none>
2020-08-25 19:48:45 +0000

    🎸🎫 Checkpoint ./src/styles/app.scss:1404815/60 ./src/styles/components/hello-world2.sc6s:1404815/72 ./src/styles/components/hello-world.scss:1404815/59 ./src/styles/components/hello-world2.scss:1404815/84 ./src/components/HelloWorld.jsx:1404815/720 ./package.json:1404815/52

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export class HelloWorld extends Component {
-    render() {
-        return (
-            <div className="hello-world">
-                <h1>Enough Babel! Don't over React! Turn down the SASS! ... Oh, hello {this.props.name}! Welcome to the Webpack!</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="hello-world">
+        <h1>
+          Enough Babel! Don't over React! Turn down the SASS! ... Oh, hello{" "}
+          {this.props.name}! Welcome to the Webpack!
+        </h1>
+      </div>
+    );
+  }
 }
 export default HelloWorld;
