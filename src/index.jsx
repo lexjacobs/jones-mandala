c37b6eff28f7e6ab7fef7325d161af2b993ef0ab
Glitch (react-webpack-babel-sass) <none>
2020-08-25 20:00:34 +0000

    🐵👽 Checkpoint ./src/styles/components/hello-world.scss:1404815/800 ./src/components/HelloWorld.jsx:1404815/1231 ./src/index.jsx:1404815/349

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld';
+import RainbowSpinner from './components/RainbowSpinner';
+import Jones from './components/Jones';
 import './styles/app.scss';
 
-const element = <HelloWorld name="Sara" />;
+var App = (props) => (
+<div>
+  <RainbowSpinner />  
+  <Jones />  
+</div>
+)
+
+const element = <App />;
 ReactDOM.render(
    element,
    document.getElementById('app')
