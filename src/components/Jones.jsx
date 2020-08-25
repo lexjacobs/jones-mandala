04b047c5811741d647f223dedf45d1bfc84a3756
Glitch (react-webpack-babel-sass) <none>
2020-08-25 20:12:38 +0000

    💵🐽 Checkpoint ./src/styles/app.scss:1404815/226 ./src/index.jsx:1404815/168 ./src/components/Jones.jsx:1404815/478 ./src/styles/components/rainbow-spinner.scss:1404815/1572 ./src/styles/components/jones.scss:1404815/753

---
+++ src/components/Jones.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+export class Jones extends Component {
+  render() {
+    return (
+      <ul>
+        <li class="rotating1"></li>
+        <li class="rotating2"></li>
+        <li class="rotating3"></li>
+        <li class="rotating4"></li>
+        <li class="rotating5"></li>
+        <li class="rotating6"></li>
+      </ul>
+    );
+  }
+}
+export default Jones;
