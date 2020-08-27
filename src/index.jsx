d3b7da984ebbd8cf80eb76a3360bed6efcc05e8a
Alex Jacobs <lex.jacobs@gmail.com>
2020-08-27 05:57:32 +0000

    🏨🎓 Checkpoint ./src/index.jsx:1404815/766 ./src/styles/components/jones.scss:1404815/15

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import RainbowSpinner from './components/RainbowSpinner';
-import Jones from './components/Jones';
-import './styles/app.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import RainbowSpinner from "./components/RainbowSpinner";
+import Jones from "./components/Jones";
+import "./styles/app.scss";
 
-var App = (props) => (
-<div>
-  <Jones />  
-</div>
-)
-<RainbowSpinner />  
+var App = props => (
+  <div>
+    <Jones />
+    <RainbowSpinner />
+  </div>
+);
 
 const element = <App />;
-ReactDOM.render(
-   element,
-   document.getElementById('app')
-);
+ReactDOM.render(element, document.getElementById("app"));
