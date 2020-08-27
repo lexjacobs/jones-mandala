3136ae042d6a0b775b35ac8ce6544297bfe14c7a
Glitch (react-webpack-babel-sass) <none>
2020-08-27 05:33:18 +0000

    🌇🐦 Checkpoint ./src/components/Jones.jsx:1404815/820 ./src/index.jsx:1404815/268 ./src/styles/components/jones.scss:1404815/1194 ./src/styles/components/rainbow-spinner.scss:1404815/18

--- src/components/Jones.jsx
+++ src/components/Jones.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
 import image from "./images";
 
-console.log(image);
-
 export class Jones extends Component {
   render() {
     return (
-      <ul>
-        <img src={image.1} className="rotating1"/>
-        <img src={image.2} className="rotating2"/>
-        <img src={image.3} className="rotating3"/>
-        <img src={image.4} className="rotating4"/>
-        <img src={image.5} className="rotating5"/>
-      </ul>
+      <React.Fragment>
+        <img className="mandala rotating" src={image.i1} />
+        <img className="mandala rotating" src={image.i2} />
+        <img className="mandala rotating" src={image.i3} />
+        <img className="mandala rotating" src={image.i4} />
+        <img className="mandala rotating" src={image.i5} />
+      </React.Fragment>
     );
   }
 }
