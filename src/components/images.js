7795d05e5ff1ad57126f4a620470d4b45259df58
Alex Jacobs <lex.jacobs@gmail.com>
2020-10-16 23:45:54 +0000

    👂🌺 Checkpoint ./.glitch-assets:1404815/2114 ./src/components/images.js:1404815/2354 ./src/styles/components/rainbow-spinner.scss:1404815/13

--- src/components/images.js
+++ src/components/images.js
@@ -1,9 +1,12 @@
 var root = "https://cdn.glitch.com/e2af77e5-5038-45be-9cb5-f60e2fa146cb%2F";
-var img5 = "jones_5.png?v=1598504334283";
-var img4 = "jones_4.png?v=1598504336344";
-var img3 = "jones_3.png?v=1598504339287";
-var img2 = "jones_3.png?v=1598504339287";
-var img1 = "jones_1.png?v=1598504337111";
+
+
+var img1 = "e_jones_pic.png?v=1602891485691";
+var img2 = "d_ring.png?v=1602891486266";
+var img3 = "c_ring.png?v=1602891488338";
+var img4 = "b_ring.png?v=1602891489485";
+var img5 = "a_raven.png?v=1602891485049";
+
 
 export default {
   i1: `${root + img1}`,
