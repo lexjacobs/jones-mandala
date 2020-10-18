e00227f048067047af504030495cc3ac16670f16
Alex Jacobs <lex.jacobs@gmail.com>
2020-10-18 19:46:30 +0000

    Rewound to commit 013d5e936071ecc40478bc8714fd782c8fe59f0c + 0 edits

--- src/components/Jones.jsx
+++ src/components/Jones.jsx
@@ -4,13 +4,13 @@ import image from "./images";
 export class Jones extends Component {
   render() {
     return (
-      <div className="jones-container">
+      <React.Fragment>
         <img className="mandala rotating" src={image.i1} />
         <img className="mandala rotating_r" src={image.i2} />
         <img className="mandala rotating" src={image.i3} />
         <img className="mandala rotating_r" src={image.i4} />
         <img className="mandala rotating" src={image.i5} />
-      </div>
+      </React.Fragment>
     );
   }
 }
--- src/components/RainbowSpinner.jsx
+++ src/components/RainbowSpinner.jsx
@@ -2,22 +2,20 @@ import React, { Component } from "react";
 export class RainbowSpinner extends Component {
   render() {
     return (
-      <div className="rainbow-container">
-        <ul>
-          <li className="rotating1"></li>
-          <li className="rotating2"></li>
-          <li className="rotating3"></li>
-          <li className="rotating4"></li>
-          <li className="rotating5"></li>
-          <li className="rotating6"></li>
-          <li className="rotating7"></li>
-          <li className="rotating8"></li>
-          <li className="rotating9"></li>
-          <li className="rotating10"></li>
-          <li className="rotating11"></li>
-          <li className="rotating12"></li>
-        </ul>
-      </div>
+      <ul>
+        <li className="rotating1"></li>
+        <li className="rotating2"></li>
+        <li className="rotating3"></li>
+        <li className="rotating4"></li>
+        <li className="rotating5"></li>
+        <li className="rotating6"></li>
+        <li className="rotating7"></li>
+        <li className="rotating8"></li>
+        <li className="rotating9"></li>
+        <li className="rotating10"></li> 
+        <li className="rotating11"></li> 
+        <li className="rotating12"></li>
+      </ul>
     );
   }
 }
