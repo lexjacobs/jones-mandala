f39e27323d02226857708606f4a52a527bcddcfb
Glitch (jones-mandala) <none>
2020-10-18 20:18:51 +0000

    🎊🏎 Checkpoint ./src/styles/components/rainbow-spinner.scss:1404815/481 ./src/components/Jones.jsx:1404815/82 ./src/components/RainbowSpinner.jsx:1404815/1543 ./src/styles/components/jones.scss:1404815/6

--- src/components/Jones.jsx
+++ src/components/Jones.jsx
@@ -4,13 +4,13 @@ import image from "./images";
 export class Jones extends Component {
   render() {
     return (
-      <React.Fragment>
+      <div className="jones-container">
         <img className="mandala rotating" src={image.i1} />
         <img className="mandala rotating_r" src={image.i2} />
         <img className="mandala rotating" src={image.i3} />
         <img className="mandala rotating_r" src={image.i4} />
         <img className="mandala rotating" src={image.i5} />
-      </React.Fragment>
+      </div>
     );
   }
 }
