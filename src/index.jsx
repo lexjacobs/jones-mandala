013d5e936071ecc40478bc8714fd782c8fe59f0c
Glitch (jones-mandala) <none>
2020-10-18 05:44:46 +0000

    📡🍍 Checkpoint ./src/styles/components/rainbow-spinner.scss:1404815/17 ./src/index.jsx:1404815/12

--- src/index.jsx
+++ src/index.jsx
@@ -26,7 +26,7 @@ var App = props => {
   } else {
     return (
       <div>
-        {view === "yes" ? <audio autoplay="true" hidden src="https://cdn.glitch.com/e2af77e5-5038-45be-9cb5-f60e2fa146cb%2Fstrawberry_edit.mp4?v=1602996276868"></audio> : null}
+        {view === "yes" ? <audio loop="true" autoplay="true" hidden src="https://cdn.glitch.com/e2af77e5-5038-45be-9cb5-f60e2fa146cb%2Fstrawberry_edit.mp4?v=1602996276868"></audio> : null}
         {/*<Jones />*/}
         <Jones />
         <RainbowSpinner />
