5e1e58a9336fb60c3f432d501f92e47b3de868db
Alex Jacobs <lex.jacobs@gmail.com>
2020-10-18 04:31:52 +0000

    ⛑😻 Checkpoint ./src/index.jsx:1404815/3747 ./package.json:1404815/3516 ./src/index.html:1404815/28

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,38 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+
 import RainbowSpinner from "./components/RainbowSpinner";
 import Jones from "./components/Jones";
 import "./styles/app.scss";
 
+var AudioChooser = props => {
+  return (
+    <div>
+      hello
+      <button>Audio</button>
+      <button>No Audio</button>
+    </div>
+  );
+};
 
-var App = props => (
-  <div>
-    <Jones />
-    <RainbowSpinner />
-  </div>
-);
+var App = props => {
+  const [view, setView] = useState("choose");
+
+  if (view === "choose") {
+    return (
+      <div>
+        <AudioChooser />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Jones />
+        <RainbowSpinner />
+      </div>
+    );
+  }
+};
 
 const element = <App />;
 ReactDOM.render(element, document.getElementById("app"));
