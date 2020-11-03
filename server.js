9390a6eb4d150d516d883c61b8b4db1603b69b64
Alex Jacobs <lex.jacobs@gmail.com>
2020-11-03 13:23:31 -0800

    Create-react-app to avoid further hair loss

--- server.js
+++
@@ -1,6 +0,0 @@
-'run strict'
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => res.sendFile('src/index.html',{root:"."}))
-app.get('/bundle', (req, res) => res.sendFile('src/output/bundle.js',{root:"."}))
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
